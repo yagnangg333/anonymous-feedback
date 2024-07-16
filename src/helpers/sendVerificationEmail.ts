3a9c654c4a02962c4d344e812eab9b589c13dda9
yagnangg333 <yagnangacharya@gmail.com>
2024-07-16 22:15:01 +0530

    small change in sendVerificationEmail

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -11,7 +11,7 @@ export async function sendVerificationEmail(
     await resend.emails.send({
       from: 'onboarding@resend.dev',
       to: email,
-      subject: 'Mystery Message Verification Code',
+      subject: 'Anonymous Feedback | Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
     });
     return { success: true, message: 'Verification email sent successfully.' };
